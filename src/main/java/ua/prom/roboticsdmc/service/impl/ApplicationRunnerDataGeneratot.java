a4de04eb4c4b48ea28888f31a155ce0485892956
Andrii Chipets <anchrobotics@gmail.com>
2023-08-23 16:57:18 +0300

    refactored magic numbers in ApplicationRunnerDataGenerator class

--- src/main/java/ua/prom/roboticsdmc/service/impl/ApplicationRunnerDataGeneratot.java
+++ src/main/java/ua/prom/roboticsdmc/service/impl/ApplicationRunnerDataGeneratot.java
@@ -28,23 +28,31 @@ public class ApplicationRunnerDataGeneratot implements ApplicationRunner {
     private final StudentDao studentDao;
     private final CourseDao courseDao;
     private final GroupDao groupDao;
+    private static final int TOTAL_STUDENT_NUMBER = 200;
+    private static final int TOTAL_GROUP_NUMBER = 10;
+    private static final int MIN_NUMBER_STUDENTS_IN_GROUP = 10;
+    private static final int MAX_NUMBER_STUDENTS_IN_GROUP = 30;
+    private static final int MIN_NUMBER_COURCES_FOR_STUDENT = 1;
+    private static final int MAX_NUMBER_COURCES_FOR_STUDENT = 2;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
 
-        List<Student> students = dataGenerator.createRandomStudent(200);
+        List<Student> students = dataGenerator.createRandomStudent(TOTAL_STUDENT_NUMBER);
         List<Course> courses = dataGenerator.createCourse();
-        List<Group> groups = dataGenerator.createRandomGroup(10);
+        List<Group> groups = dataGenerator.createRandomGroup(TOTAL_GROUP_NUMBER);
         studentDao.saveAll(students);
         courseDao.saveAll(courses);
         groupDao.saveAll(groups);
         groups = new ArrayList<>(groupDao.findAll());
         students = new ArrayList<>(studentDao.findAll());
-        students = dataGenerator.assignStudentToGroup(groups, students, 10, 30);
+        students = dataGenerator.assignStudentToGroup(groups, students, MIN_NUMBER_STUDENTS_IN_GROUP,
+                MAX_NUMBER_STUDENTS_IN_GROUP);
         studentDao.distributeStudentsToGroups(students);
         students = studentDao.findAll();
         courses = courseDao.findAll();
-        List<List<Integer>> studentsAssignedToCourses = dataGenerator.assignStudentToCourses(students, courses, 1, 3);
+        List<List<Integer>> studentsAssignedToCourses = dataGenerator.assignStudentToCourses(students, courses,
+                MIN_NUMBER_COURCES_FOR_STUDENT, MAX_NUMBER_COURCES_FOR_STUDENT);
         courseDao.fillRandomStudentCourseTable(studentsAssignedToCourses);
     }
 }

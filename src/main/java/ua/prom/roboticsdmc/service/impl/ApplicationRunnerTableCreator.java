c1b3eb6a735de33b80aa4201bcfb24e621a2a363
Andrii Chipets <anchrobotics@gmail.com>
2023-08-28 18:55:06 +0300

    added logback-spring.xml file configuration, refactored ApplicationRunnerTableCreator class

--- src/main/java/ua/prom/roboticsdmc/service/impl/ApplicationRunnerTableCreator.java
+++ src/main/java/ua/prom/roboticsdmc/service/impl/ApplicationRunnerTableCreator.java
@@ -3,7 +3,6 @@ package ua.prom.roboticsdmc.service.impl;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import lombok.AllArgsConstructor;
@@ -12,12 +11,12 @@ import ua.prom.roboticsdmc.service.TableCreator;
 
 @Profile("!test")
 @Component
-@Order(1)
 @AllArgsConstructor
 public class ApplicationRunnerTableCreator implements ApplicationRunner {
 
     private static final String SCHEMA_FILE_PATH = "src/main/resources/sgl/schema.sql";
     private final TableCreator tableCreator;
+    private final TableFiller tableFiller;
     private final UserDao userDao;
 
     @Override
@@ -25,6 +24,7 @@ public class ApplicationRunnerTableCreator implements ApplicationRunner {
 
         if (userDao.isAnyTableInDbSchema()) {
             tableCreator.createTables(SCHEMA_FILE_PATH);
+            tableFiller.fillData();
         }
     }
 }
